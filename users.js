4bda0a3405d1d375b7831d6d3dde7d26fec0d624
Julia Aquino <eispetbirb0626@gmail.com>
2025-11-27 02:34:50 +0800

    Add web

---
+++ users.js
@@ -0,0 +1,50 @@
+var users = [
+    {
+        "name": "Maraiah Queen Jaraula Arceta",
+        "description": "Main Rapper, Visual",
+        "nickname": "Aiah",
+        "image": "https://bini.abs-cbn.com/_next/image?url=https%3A%2F%2Fartist-images.abs-cbn.com%2Fwp-content%2Fuploads%2F2025%2F11%2F20205757%2FAIAH_1080x1080px.png&w=1080&q=75"
+    },
+    {
+        "name": "Ma. Nicolette Florenosos Vergara",
+        "description": "Main Vocal, Lead Dancer, Lead Rapper",
+        "nickname": "Colet",
+        "image": "https://bini.abs-cbn.com/_next/image?url=https%3A%2F%2Fartist-images.abs-cbn.com%2Fwp-content%2Fuploads%2F2025%2F11%2F20205814%2FCOLET_1080x1080px.png&w=1080&q=75"
+    },
+    {
+        "name": "Mary Loi Yves Kipte Ricalde",
+        "description": "Main Vocal",
+        "nickname": "Maloi",
+        "image": "https://bini.abs-cbn.com/_next/image?url=https%3A%2F%2Fartist-images.abs-cbn.com%2Fwp-content%2Fuploads%2F2025%2F11%2F20205843%2FMALOI_1080x1080px.png&w=1080&q=75"
+    },
+    {
+        "name": "Gweneth Llaguno Apuli",
+        "description": "Lead Vocal, Lead Rapper",
+        "nickname": "Gwen",
+        "image": "https://bini.abs-cbn.com/_next/image?url=https%3A%2F%2Fartist-images.abs-cbn.com%2Fwp-content%2Fuploads%2F2025%2F11%2F20205828%2FGWEN_1080x1080px.png&w=1080&q=75"
+    },
+    {
+        "name": "Lindtsey Stacey Aubrey Casumpang Sevilleja",
+        "description": "Main Rapper, Lead Dancer, Sub Vocalist ",
+        "nickname": "Stacey",
+        "image": "https://bini.abs-cbn.com/_next/image?url=https%3A%2F%2Fartist-images.abs-cbn.com%2Fwp-content%2Fuploads%2F2025%2F11%2F20205953%2FSTACEY_1080x1080px.jpg&w=1080&q=75"
+    },
+    {
+        "name": "Mikhaela Janna Jimenea Lim",
+        "description": "Main Rapper, Lead Dancer, Visual",
+        "nickname": "Mikha",
+        "image": "https://bini.abs-cbn.com/_next/image?url=https%3A%2F%2Fartist-images.abs-cbn.com%2Fwp-content%2Fuploads%2F2025%2F11%2F20205925%2FMIKHA_1080x1080px.png&w=1080&q=75"
+    },
+    {
+        "name": "Jhoanna Christine Burgos Robles",
+        "description": "Leader, Lead Vocal, Lead Rapper",
+        "nickname": "Jhoanna",
+        "image": "https://bini.abs-cbn.com/_next/image?url=https%3A%2F%2Fartist-images.abs-cbn.com%2Fwp-content%2Fuploads%2F2025%2F11%2F20205906%2FJHOANNA_1080x1080px.png&w=1080&q=75"
+    },
+    {
+        "name": "Sheena Mae Manuel Catacutan",
+        "description": "Main Dancer, Youngest",
+        "nickname": "Sheena",
+        "image": "https://bini.abs-cbn.com/_next/image?url=https%3A%2F%2Fartist-images.abs-cbn.com%2Fwp-content%2Fuploads%2F2025%2F11%2F20205941%2FSHEENA_1080x1080px.png&w=1080&q=75"
+    }
+]
